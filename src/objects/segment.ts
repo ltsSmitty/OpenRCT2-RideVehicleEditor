893d0025a6335fdf629c50290a44e4ec864bd123
Smitty Penman <smitty.penman@gmail.com>
2022-11-02 13:06:57 -0400

    update Segment class and SegmentDescriptor type

--- src/objects/segment.ts
+++ src/objects/segment.ts
@@ -1,41 +1,33 @@
 import { TrackElementType } from '../utilities/trackElementType';
 import { RideType } from '../utilities/rideType';
 
-export class Segment {
+export type SegmentDescriptor = {
     location: CoordsXYZD;
     ride: number; // will log an error if you specify a ride # that doesn't exist
     trackType: TrackElementType; // e.g. TrackElementType.LeftBankedDown25ToDown25
     rideType: RideType;
+}
 
-    constructor(loc: CoordsXYZD, r: number, tType: TrackElementType, rType: RideType) {
-        this.location = loc;
-        this.ride = r;
-        this.trackType = tType;
-        this.rideType = rType;
+export class Segment {
+    private _location: CoordsXYZD;
+    private _ride: number; // will log an error if you specify a ride # that doesn't exist
+    private _trackType: TrackElementType; // e.g. TrackElementType.LeftBankedDown25ToDown25
+    private _rideType: RideType;
+
+    constructor(location: CoordsXYZD, ride: number, trackType: TrackElementType, rideType: RideType) {
+        this._location = location;
+        this._ride = ride;
+        this._trackType = trackType;
+        this._rideType = rideType;
     }
 
-    public get() {
+    public get(): SegmentDescriptor {
         return {
-            location: this.location,
-            ride: this.ride,
-            trackType: this.trackType,
-            rideType: this.rideType
+            location: this._location,
+            ride: this._ride,
+            trackType: this._trackType,
+            rideType: this._rideType
         };
     }
 }
 
-export type trackPlaceInstructions = {
-    brakeSpeed?: number;
-    colour?: number;
-    seatRotation?: number | null;
-    trackPlaceFlags?: number; // the ghost flag is 104
-    isFromTrackDesign?: boolean; // default is false
-    flags?: typeof Flags;
-};
-
-export type trackRemoveInstructions = {
-    buildLocation: CoordsXYZD,
-    trackType: TrackElementType,
-    sequence: number, // not really sure what this is
-    flags: typeof Flags
-};
