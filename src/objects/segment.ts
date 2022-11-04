4da0b9dcc64017817cfeeef5c7df0cc2f0eb1199
Smitty Penman <smitty.penman@gmail.com>
2022-11-04 12:59:15 -0400

    Add nextLocation and previousLocation to segment

--- src/objects/segment.ts
+++ src/objects/segment.ts
@@ -1,5 +1,6 @@
 import { TrackElementType } from '../utilities/trackElementType';
 import { RideType } from '../utilities/rideType';
+import * as finder from "../services/trackElementFinder"
 
 export type SegmentDescriptor = {
     location: CoordsXYZD;
@@ -13,12 +14,19 @@ export class Segment {
     private _ride: number; // will log an error if you specify a ride # that doesn't exist
     private _trackType: TrackElementType; // e.g. TrackElementType.LeftBankedDown25ToDown25
     private _rideType: RideType;
+    nextLocation!: CoordsXYZD | null;
+    previousLocation!: CoordsXYZD | null;
 
     constructor(segment: SegmentDescriptor) {
         this._location = segment.location;
         this._ride = segment.ride;
         this._trackType = segment.trackType;
         this._rideType = segment.rideType;
+
+        // set the next and previous locations
+        const thisTI = finder.getTIAtSegment(this);
+        this.nextLocation = thisTI?.nextPosition || null;
+        this.previousLocation = thisTI?.previousPosition || null;
     }
 
     public get(): SegmentDescriptor {
