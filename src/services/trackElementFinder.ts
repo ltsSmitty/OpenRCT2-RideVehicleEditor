db56d6791791ad0c6f162f8e34cee967be865cd2
Smitty Penman <smitty.penman@gmail.com>
2022-11-06 06:08:29 -0500

    attemping to change z filtering

--- src/services/trackElementFinder.ts
+++ src/services/trackElementFinder.ts
@@ -1,3 +1,4 @@
+import { highlightMapRange } from './highlightGround';
 import { TileElementItem, TrackElementItem } from './SegmentController';
 import { debug } from "../utilities/logger";
 import { Segment } from '../objects/segment';
@@ -59,7 +60,7 @@ const getSegmentFromTrackElement = (e: TileElementItem<TrackElement>): Segment |
         location: tempTI.position,
         trackType: tempTI.segment.type,
         rideType: e.element.rideType,
-        ride: e.element.ride,
+        ride: e.element.ride
     });
 };
 
@@ -74,41 +75,74 @@ export const getSurfaceElementsFromCoords = (coords: CoordsXY | CoordsXYZ | Coor
  */
 export const getSpecificTrackElement = (ride: number, coords: CoordsXYZD): TrackElementItem => {
     const trackELementsOnTile = getTrackElementsFromCoords({ x: coords.x, y: coords.y });
+    debug(`Getting the track elements at coords ${coords.x}, ${coords.y}..
+    ${trackELementsOnTile.length} total track element exists on this tile.`);
     // make sure the ride matches this ride
     const trackForThisRide = trackELementsOnTile.filter(e => e.element.ride === ride);
-
+    debug(`${trackForThisRide.length} of those track elements are for this ride.`);
     // if there are two segments for the same ride in this tile, make sure it's the proper one
-    const chosenTrack = trackForThisRide.filter(t => t.element.baseZ === coords.z);
-    if (chosenTrack.length > 1) console.log(`Error: There are two overlapping elements at this tile with the same XYZ. Returning the first.`);
-    return chosenTrack[0];
+    if (trackForThisRide.length > 1) {
+        const chosenTrack = trackForThisRide.filter(t => (t.element.baseZ === coords.z && t.element.direction === coords.direction));
+        if (chosenTrack.length > 1) console.log(`Error: There are two overlapping elements at this tile with the same XYZD. Returning the 0th.`);
+        return chosenTrack[0];
+    }
+    return trackForThisRide[0];
 };
 
 export const doesSegmentExistHere = (selectedSegment: Segment | null): { exists: false | "ghost" | "real", element: TrackElementItem | null } => {
 
-    if (selectedSegment == null || selectedSegment.nextLocation == null) return { exists: false, element: null };
+    if (selectedSegment == null || selectedSegment.nextLocation() == null) {
+        debug(`${selectedSegment == null ? "selectedSegment is null" : "selectedSegment.nextLocation() is null"}`);
+        return { exists: false, element: null };
+    }
 
-    const { x, y, z, direction } = selectedSegment.nextLocation; // location of next track element
+    const { x, y, z } = selectedSegment.nextLocation()!; // location of next track element
     const trackELementsOnNextTile = getTrackElementsFromCoords({ x, y });
     // make sure the ride matches this ride
     const trackForThisRide = trackELementsOnNextTile.filter(e => e.element.ride === selectedSegment.get().ride);
-
+    debug(`There are ${trackForThisRide.length} track elements for this ride on the next tile.`);
     // if there are two segments for the same ride in this tile, make sure it's the proper one
-    if (trackForThisRide.length > 1) console.log(`Error: There are two overlapping elements at this tile with the same XYZ. Returning the first.`);
-    const chosenTrack = trackForThisRide.filter(t => t.element.baseZ === z);
-    const thisSegmentELement = chosenTrack[0];
+    let thisTrack: TrackElementItem;
+    if (trackForThisRide.length > 1) {
+        console.log(`Error: There are two overlapping elements at this tile with the same XYZ. Returning the first.`);
+        const chosenTrack = trackForThisRide.filter(t => t.element.baseZ === z);
+        thisTrack = chosenTrack[0];
+    } else { thisTrack = trackForThisRide[0]; }
+    debug(`thisTrack: ${JSON.stringify(thisTrack)}`);
 
 
-    if (!thisSegmentELement) return { exists: false, element: null };
+    if (!thisSegmentELement) {
+        debug(`searched for tracks on this ride, but apparently there were none?`);
+        return { exists: false, element: null };
+    }
+
     // TODO make sure this actually checks. Otherwise going to have to attempt to build and get the flags? Or can query the segmentMap
     debug(`A segment for ride ${selectedSegment.get().ride} exists at (${x}, ${y}). \t
     Note the segment in the next tile has a z value of ${thisSegmentELement.segment?.get().location.z} compared to the current segment's z value of ${selectedSegment.get().location.z}.
     Are the z values equal: ${thisSegmentELement.segment?.get().location.z === selectedSegment.get().location.z}`);
+
     if (thisSegmentELement.element.isGhost) return { exists: "ghost", element: thisSegmentELement };
     return { exists: "real", element: thisSegmentELement };
+    // // if there are two segments for the same ride in this tile, make sure it's the proper one
+    // if (trackForThisRide.length > 1) {
+    //     console.log(`Error: There are two overlapping elements at this tile with the same XYZ. Returning the first.`);
+    //     // this filter isn't working with down-facing tracks
+    //     const chosenTrack = trackForThisRide.filter(t => t.element.baseZ === z);
+    //     thisTrack = chosenTrack[0];
+    // } else thisTrack = trackForThisRide[0];
+    // debug(`number of tracks for this ride: ${trackForThisRide.length}`);
+
+    // // TODO make sure this actually checks. Otherwise going to have to attempt to build and get the flags? Or can query the segmentMap
+    // if (thisTrack.element.isGhost) return { exists: "ghost", element: thisTrack };
+    // return { exists: "real", element: thisTrack };
+
 };
+
 
 /**
  * Since stalls are also considered rides, use this filter to check stall vs true ride
+ *
+ *
  * @param rideNumber  @returns true if stall, false if other kind of ride.
  */
 const isRideAStall = (rideNumber: number): boolean => {
@@ -121,6 +155,7 @@ export const getTIAtSegment = (segment: Segment | null): TrackIterator | null =>
         debug(`segment was null`);
         return null;
     }
+    debug(`Getting specific track element of ride ${segment.get().ride} at (${segment.get().location.x}, ${segment.get().location.y})`);
     const thisSegmentIndex = getSpecificTrackElement(segment.get().ride, segment.get().location).index; // needed for iterator
     const newTI = map.getTrackIterator(<CoordsXY>segment.get().location, thisSegmentIndex); // set up TI
 
